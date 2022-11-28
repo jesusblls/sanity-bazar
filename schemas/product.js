9c194747f75f02dbf38f0cc9e636ec66bbbf40f9
Jesús Castillo <jesusblls@iMac-Pro-de-Jesus.local>
2022-11-27 21:35:42 -0600

    initial commit

---
+++ schemas/product.js
@@ -0,0 +1,48 @@
+export default {
+    name: 'product',
+    type: 'document',
+    title: 'Product',
+    fields: [
+        {
+            name: 'name',
+            type: 'string',
+            title: 'Name'
+        },
+        {
+            name: 'price',
+            type: 'number',
+            title: 'Price'
+        },
+        {
+            name: 'category',
+            type: 'reference',
+            title: 'Category',
+            to: [{ type: 'category' }]
+        },
+        {
+            name: 'images',
+            type: 'array',
+            title: 'Images',
+            of: [
+                {
+                    name: 'image',
+                    type: 'image',
+                    title: 'Image',
+                    options: {
+                        hotspot: true,
+                    },
+                    fields: [
+                        {
+                            name: 'alt',
+                            type: 'string',
+                            title: 'Alternative text',
+                        },
+                    ],
+                },
+            ],
+            options: {
+                layout: 'grid',
+            },
+        }
+    ]
+}
